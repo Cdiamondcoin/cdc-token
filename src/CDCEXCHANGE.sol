29c34cc8d92d047283ce47bf4f963eb79d0a3e1c
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-04-16 10:06:03 +0300

    Remove Medianizer, set rate manually, improve deploy script

--- src/CDCEXCHANGE.sol
+++ src/CDCEXCHANGE.sol
@@ -6,9 +6,6 @@ import "ds-token/token.sol";
 import "ds-stop/stop.sol";
 import "ds-note/note.sol";
 
-contract MedianizerLike {
-    function peek() external view returns (bytes32, bool);
-}
 
 /**
  * @title CDC
@@ -20,31 +17,20 @@ contract CDCEXCHANGEEvents {
         address sender,
         uint ethValue,
         uint cdcValue,
-        uint ethUsdRate,
-        uint cdcUsdRate
+        uint rate
     );
-    event LogFeedValid(bool feedValid);
 }
 
 contract CDCEXCHANGE is DSAuth, DSStop, DSMath, CDCEXCHANGEEvents {
-    uint public cdcUsdRate;              //usd price of 1 CDC token. 18 digit precision
-    uint public ethUsdRate;              //price of ETH in USD. 18 digit precision
-    MedianizerLike public priceFeed;     //address of the Makerdao price feed
-    bool public feedValid;               //if true feed has valid USD/ETH rate
-    ERC20 public dpt;                    //CDC token contract
     ERC20 public cdc;                    //CDC token contract
-    bool public manualUsdRate = true;    //if true enables token buy even if priceFeed does not provide valid data
-    uint public minDptInvestmentAmount = 0; //minimal amount of CDC to buy, if equal with 0 than validation is disabled
+    uint public rate;                    //price of 1 CDC token. 18 digit precision
 
     /**
     * @dev Constructor
     */
-    constructor(address cdc_, address dpt_, address priceFeed_, uint cdcUsdRate_, uint ethUsdRate_) public {
+    constructor(address cdc_, uint rate_) public {
         cdc = ERC20(cdc_);
-        dpt = ERC20(dpt_);
-        priceFeed = MedianizerLike(priceFeed_);
-        cdcUsdRate = cdcUsdRate_;
-        ethUsdRate = ethUsdRate_;
+        rate = rate_;
     }
 
     /**
@@ -58,95 +44,21 @@ contract CDCEXCHANGE is DSAuth, DSStop, DSMath, CDCEXCHANGEEvents {
     * @dev Low level token purchase function.
     */
     function buyTokens() public payable stoppable {
-        uint tokens;
-        bool feedValidSave = feedValid;
-        bytes32 ethUsdRateB;
-
         require(msg.value != 0, "Invalid amount");
 
-        // receive ETH/USD price from external feed
-        (ethUsdRateB, feedValid) = priceFeed.peek();
-
-        // emit LogFeedValid event if validity of feed changes
-        if (feedValidSave != feedValid) {
-            emit LogFeedValid(feedValid);
-        }
-
-        // if feed is valid, load ETH/USD rate from it
-        if (feedValid) {
-            ethUsdRate = uint(ethUsdRateB);
-        } else {
-            // if feed invalid revert if manualUSDRate_ is NOT allowed
-            require(manualUsdRate, "Manual rate not allowed");
-        }
-
-        tokens = wdiv(wmul(ethUsdRate, msg.value), cdcUsdRate);
+        uint tokens;
+        tokens = wdiv(msg.value, rate);
 
         address(owner).transfer(msg.value);
         cdc.transferFrom(owner, msg.sender, tokens);
-        emit LogBuyToken(owner, msg.sender, msg.value, tokens, ethUsdRate, cdcUsdRate);
+        emit LogBuyToken(owner, msg.sender, msg.value, tokens, rate);
     }
 
     /**
-    * @dev Get tokenAmount price in ETH
+    * @dev Set exchange rate CDC/ETH value.
     */
-    function getPrice(uint tokenAmount) public view returns (uint) {
-        bool feedValid_;
-        uint ethUsdRate_;
-        bytes32 ethUsdRateB;
-        require(tokenAmount > 0, "Invalid amount");
-
-        // receive ETH/USD price from external feed
-        (ethUsdRateB, feedValid_) = priceFeed.peek();
-
-        if (feedValid_) {
-            ethUsdRate_ = uint(ethUsdRateB);
-        } else {
-            // load manual ETH/USD rate if enabled
-            require(manualUsdRate, "Manual rate not allowed");
-            ethUsdRate_ = ethUsdRate;
-        }
-
-        return wdiv(wmul(tokenAmount, cdcUsdRate), ethUsdRate_);
-    }
-
-    /**
-    * @dev Set exchange rate CDC/USD value.
-    */
-    function setCdcRate(uint cdcUsdRate_) public auth note {
-        require(cdcUsdRate_ > 0, "Invalid amount");
-        cdcUsdRate = cdcUsdRate_;
-    }
-
-    /**
-    * @dev Set exchange rate CDC/ETH value manually.
-    *
-    * This function should only be used if the priceFeed does not return
-    * valid price data.
-    *
-    */
-    function setEthRate(uint ethUsdRate_) public auth note {
-        require(manualUsdRate, "Manual rate not allowed");
-        ethUsdRate = ethUsdRate_;
-    }
-
-    /**
-    * @dev Set the price feed
-    */
-    function setPriceFeed(address priceFeed_) public auth note {
-        require(priceFeed_ != 0x0, "Wrong PriceFeed address");
-        priceFeed = MedianizerLike(priceFeed_);
-    }
-
-    /**
-    * @dev Set manual feed update
-    *
-    * If `manualUsdRate` is true, then `buyTokens()` will calculate the CDC amount based on latest valid `ethUsdRate`,
-    * so `ethUsdRate` must be updated by admins if priceFeed fails to provide valid price data.
-    *
-    * If manualUsdRate is false, then buyTokens() will simply revert if priceFeed does not provide valid price data.
-    */
-    function setManualUsdRate(bool manualUsdRate_) public auth note {
-        manualUsdRate = manualUsdRate_;
+    function setRate(uint rate_) public auth note {
+        require(rate_ > 0, "Invalid amount");
+        rate = rate_;
     }
 }
--- src/CDCEXCHANGE.t.sol
+++ src/CDCEXCHANGE.t.sol
@@ -5,27 +5,6 @@ import "ds-math/math.sol";
 import "./CDCEXCHANGE.sol";
 import "./CDC.sol";
 
-contract TestMedianizerLike {
-    bytes32 public ethUsdRate;
-    bool public feedValid;
-
-    constructor(uint ethUsdRate_, bool feedValid_) public {
-        ethUsdRate = bytes32(ethUsdRate_);
-        feedValid = feedValid_;
-    }
-
-    function setEthUsdRate(uint ethUsdRate_) public {
-        ethUsdRate = bytes32(ethUsdRate_);
-    }
-
-    function setValid(bool feedValid_) public {
-        feedValid = feedValid_;
-    }
-
-    function peek() external view returns (bytes32, bool) {
-        return (ethUsdRate,feedValid);
-    }
-}
 
 contract CDCEXCHANGETester {
     CDCEXCHANGE public _exchange;
@@ -45,20 +24,15 @@ contract CDCEXCHANGETester {
 contract CDCEXCHANGETest is DSTest, DSMath, CDCEXCHANGEEvents {
     uint constant CDC_SUPPLY = (10 ** 7) * (10 ** 18);
     CDC cdc;
-    CDC dpt;
-    TestMedianizerLike feed;
     CDCEXCHANGE exchange;
     CDCEXCHANGETester user;
-    uint etherBalance ;
+    uint etherBalance;
     uint sendEth;
-    uint ethUsdRate = 317.96 ether;
-    uint cdcUsdRate = 3.5 ether;
+    uint rate = 0.5 ether;
 
     function setUp() public {
         cdc = new CDC();
-        dpt = new CDC();
-        feed = new TestMedianizerLike(ethUsdRate, true);
-        exchange = new CDCEXCHANGE(cdc, dpt, feed, cdcUsdRate, ethUsdRate);
+        exchange = new CDCEXCHANGE(cdc, rate);
         user = new CDCEXCHANGETester(exchange);
         cdc.approve(exchange, uint(-1));
         require(cdc.balanceOf(this) == CDC_SUPPLY);
@@ -82,7 +56,7 @@ contract CDCEXCHANGETest is DSTest, DSMath, CDCEXCHANGEEvents {
     function testOthersBuyTenTokens() public {
         sendEth = 10 ether;
         user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
+        assertEq(cdc.balanceOf(this),CDC_SUPPLY - 20 ether);
     }
 
     function testFailBuyTenTokensIfExchangeStopped() public {
@@ -116,136 +90,20 @@ contract CDCEXCHANGETest is DSTest, DSMath, CDCEXCHANGEEvents {
     function testBuyTokens() public {
         sendEth = 10 ether;
         user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
+        assertEq(cdc.balanceOf(this), CDC_SUPPLY - 20 ether);
+        assertEq(cdc.balanceOf(user), 20 ether);
+    }
+
+    function testBuyTokensSetRate() public {
+        sendEth = 0.18447 ether;
+        exchange.setRate(sendEth);
+        user.doBuyTokens(sendEth);
+        assertEq(cdc.balanceOf(this), CDC_SUPPLY - 1 ether);
+        assertEq(cdc.balanceOf(user), 1 ether);
     }
 
     function testFailBuyTokensZeroAmount() public {
         sendEth = 0 ether;
         user.doBuyTokens(sendEth);
-    }
-
-    function testBuyTokensSetCdcUsdRate() public {
-        sendEth = 10 ether;
-        cdcUsdRate = 5 ether;
-        exchange.setCdcRate(cdcUsdRate);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
-    }
-
-    function testFailBuyTokensSetZeroCdcUsdRate() public {
-        exchange.setCdcRate(0);
-    }
-
-    function testBuyTokensSetEthUsdRate() public {
-        sendEth = 10 ether;
-        ethUsdRate = 300 ether;
-        feed.setEthUsdRate(ethUsdRate);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
-    }
-
-    function testBuyTokensSetEthUsdAndCdcUsdRate() public {
-        sendEth = 10 ether;
-        ethUsdRate = 300 ether;
-        cdcUsdRate = 5 ether;
-        exchange.setCdcRate(cdcUsdRate);
-        feed.setEthUsdRate(ethUsdRate);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
-    }
-
-    function testBuyTenTokensSetEthUsdAndCdcUsdRateStatic() public {
-        exchange.setCdcRate(5 ether);
-        feed.setEthUsdRate(20 ether);
-        user.doBuyTokens(10.5 ether);
-        assertEq(cdc.balanceOf(user), 42 ether);
-    }
-
-    function testBuyTokenSetManualEthUsdRateUpdate() public {
-        sendEth = 10 ether;
-        ethUsdRate = 300 ether;
-        feed.setValid(false);
-        exchange.setManualUsdRate(true);
-        exchange.setEthRate(ethUsdRate);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
-    }
-
-    function testFailBuyTokenSetManualEthUsdRateUpdateIfManuaUsdRatelIsFalse() public {
-        sendEth = 10 ether;
-        ethUsdRate = 300 ether;
-        exchange.setEthRate(ethUsdRate);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(this),CDC_SUPPLY - (wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate)));
-    }
-
-    function testInvalidFeedDoesNotUpdateEthUsdRate() public {
-        sendEth = 134 ether;
-        uint feedEthUsdRate = 1400 ether; //should not equal to `ethUsdRate` if you want a reasonable test
-        exchange.setManualUsdRate(true);
-        feed.setValid(false);
-        feed.setEthUsdRate(feedEthUsdRate);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(user), wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate));
-    }
-
-    function testFailInvalidFeedFailsIfManualUpdateIsFalse() public {
-        sendEth = 134 ether;
-        exchange.setManualUsdRate(false);
-        feed.setValid(false);
-        user.doBuyTokens(sendEth);
-    }
-
-    function testSetFeed() public {
-        sendEth = 10 ether;
-        ethUsdRate = 2000 ether;
-        TestMedianizerLike feed1 = new TestMedianizerLike(ethUsdRate, true);
-        exchange.setPriceFeed(feed1);
-        user.doBuyTokens(sendEth);
-        assertEq(cdc.balanceOf(user), wdiv(wmul(ethUsdRate, sendEth), cdcUsdRate));
-    }
-
-    function testFailSetZeroAddressFeed() public {
-        TestMedianizerLike feed1;
-        exchange.setPriceFeed(feed1);
-    }
-
-    function testFailZeroTokenGetPrice() public view {
-        exchange.getPrice(0);
-    }
-
-    function testValidGetPriceWithFeedPrice() public {
-        ethUsdRate = 350 ether;
-        cdcUsdRate = 3.5 ether;
-        assertEq(exchange.getPrice(100), 1);
-    }
-
-    function testGetPriceDecimalWithFeedPrice() public {
-        ethUsdRate = 350 ether;
-        cdcUsdRate = 3.5 ether;
-        feed.setEthUsdRate(ethUsdRate);
-        assertEq(exchange.getPrice(275 ether), 2.75 ether);
-    }
-
-    function testGetPriceDecimalMoreAfterCommaWithFeedPrice() public {
-        ethUsdRate = 299.99 ether;
-        cdcUsdRate = 3.5 ether;
-        feed.setEthUsdRate(ethUsdRate);
-        assertEq(exchange.getPrice(32 ether), 0.373345778192606420 ether);
-    }
-
-    function testGetPriceWithManualRate() public {
-        ethUsdRate = 157.5 ether;
-        cdcUsdRate = 3.5 ether;
-        feed.setValid(false);
-        exchange.setEthRate(ethUsdRate);
-        exchange.setManualUsdRate(true);
-        assertEq(exchange.getPrice(31 ether), 0.688888888888888889 ether);
-    }
-
-    function testFailGetPriceWithInvalidFeedAndDisabledManualRate() public {
-        exchange.setManualUsdRate(false);
-        feed.setValid(false);
-        exchange.getPrice(100);
     }
 }
